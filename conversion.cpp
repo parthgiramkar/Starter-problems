17135c58bb031ec5520673db2155901a0d4dcde1
parthgiramkar <imparthgiramkar@gmail.com>
2025-01-04 12:19:30 +0530

    Create conversion.cpp

---
+++ conversion.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int c;
+    cout<<"enter the height in centimeters - "<<endl;
+    cin>>c;
+    
+    int i=c*0.394;
+    cout<<"the height in inches is - "<<i<<endl;
+    
+    int f=i/12;
+    cout<<"the height in feet is - " <<f<<endl;
+   
+       
+}
