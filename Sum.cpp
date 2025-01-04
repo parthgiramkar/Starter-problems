13567c370b0c1b1df5fa8ddb670b3457777e857c
parthgiramkar <imparthgiramkar@gmail.com>
2025-01-04 12:16:14 +0530

    Create Sum.cpp

---
+++ Sum.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    
+    /*
+    int a;
+    float b;
+    
+    cout<<"enter first no. - ";
+    cin>>a;
+    cout<<endl;
+   
+    
+    cout<<"enter second no. - ";
+    cin>>b;
+    cout<<endl;
+    cout<<"the sum - "<<b+a; 
+    
+    return 0;
+    */
+    
+    int sum;
+    for(int i=0;i<11;i++) {
+        sum=sum+i;
+    }
+    cout<<"the sum of number are - "<<sum;
+    
+    
+    
+}
