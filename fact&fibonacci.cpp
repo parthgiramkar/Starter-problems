4f840afd0cca561aed05226f123a41a369f71047
parthgiramkar <imparthgiramkar@gmail.com>
2025-01-07 15:25:56 +0530

    Update fact&fibonacci.cpp

--- fact&fibonacci.cpp
+++ fact&fibonacci.cpp
@@ -20,3 +20,35 @@ int main() {
     return 0;
 }
 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"enter number - ";
+    cin>>n;
+    
+    if(n==1) {
+        cout<<"the value is 0 "<<endl;
+    }
+    else if(n==2) {
+        cout<<"the value is 1 "<<endl;
+    }
+    else {
+        int next, first=0 , second=1;
+        cout<<"Fibonacci sequence - "<<first<<" "<< second<<" ";
+        for(int i=3;i<=n;i++) {
+                next=first + second ;
+                first=second;
+                second=next;
+                cout<<next<<" ";
+        }
+        cout<<endl;
+        cout<<"the value of "<< n <<" fibonacci number is "<<next;
+    
+    
+    }
+
+
+}
