5d38c969e46f827ffef146e77eb4defb48766fb5
parthgiramkar <imparthgiramkar@gmail.com>
2025-01-08 12:58:54 +0530

    Update divisor.cpp

--- divisor.cpp
+++ divisor.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+
+int main() {
+    int n;
+    cout<<"enter number to find its divisors - "<<endl;
+    cin>>n;
+   
+    for(int i=1;i<n;i++) {
+        if(i==0) {
+            cout<<"-_-"<<endl;
+        }
+        else if(n%i==0 || i==1) {
+            cout<<i<<"  is divisor of "<<n<<endl;
+        }
+    }
+   
+   
+}
